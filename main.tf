ed4006b8e7890dea8be322e7a96506586d29f1ff
Balázs Buri <burib@users.noreply.github.com>
2025-01-04 18:40:14 +0100

    feat: save hosted zone id into ssm

--- main.tf
+++ main.tf
@@ -29,6 +29,13 @@ resource "aws_route53_record" "wildcard_cert_validation" {
   ttl = "60"
 }
 
+resource "aws_ssm_parameter" "hosted_zone_id" {
+  name        = "/zones/${replace(aws_route53_zone.this.name, ".", "-DOT-")}/id"
+  value       = aws_route53_zone.this.zone_id
+  description = "Route53 Hosted Zone ID for ${aws_route53_zone.this.name}"
+  type        = "String"
+}
+
 resource "aws_ssm_parameter" "wildcard_cert_arn" {
   name        = "/certificates/${replace(aws_route53_zone.this.name, ".", "-DOT-")}/wildcard/arn"
   value       = aws_acm_certificate.wildcard_cert.arn
